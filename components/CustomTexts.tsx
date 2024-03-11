c339c7abcce01bde19e694b3d521a7fcd46b98a8
Ross Tungol <ross@proudcloud.io>
2024-03-11 20:05:52 +0800

    Updates TypingText component content and styling

--- components/CustomTexts.tsx
+++ components/CustomTexts.tsx
@@ -1,5 +1,26 @@
 "use client"
 
-export const TypingText = () => <p>Typing Text</p>
+import { motion } from "framer-motion"
+import { textContainer, textVariant2 } from "@/utils/motions"
 
-export const TitleText = () => <h2>Title Text</h2>
+type CustomTextProps = {
+  title: string
+  textStyles: string
+}
+
+export const TypingText = ({ title, textStyles }: CustomTextProps) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((letter, index) => (
+      <motion.span variants={textVariant2} key={index}>
+        {letter === " " ? "\u00A0" : letter}
+      </motion.span>
+    ))}
+  </motion.p>
+)
+
+export const TitleText = ({ title, textStyles }: CustomTextProps) => (
+  <h2>Title Text</h2>
+)
