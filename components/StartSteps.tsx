f8cc619c2df0342d3e0985b6fdc863db427590ac
Ross Tungol <ross@proudcloud.io>
2024-03-14 17:04:58 +0800

    Fixes hex code format

--- components/StartSteps.tsx
+++ components/StartSteps.tsx
@@ -12,7 +12,7 @@ const StartSteps = ({ number, text }: StartStepsProps) => (
     >
       <p className='font-bold text-[20px] text-white'>0{number}</p>
     </div>
-    <p className='flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32px]'>
+    <p className='flex-1 ml-[30px] font-normal text-[18px] text-[#b0b0b0] leading-[32px]'>
       {text}
     </p>
   </div>
