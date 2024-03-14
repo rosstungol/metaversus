6658f8d031f33c2778c104b40e468f537dfb8744
Ross Tungol <ross@proudcloud.io>
2024-03-14 17:07:52 +0800

    Updates World component content and styling

--- sections/World.tsx
+++ sections/World.tsx
@@ -1,5 +1,53 @@
 "use client"
 
-const World = () => <section>World section</section>
+import { motion } from "framer-motion"
+import { TitleText, TypingText } from "@/components"
+import { staggerContainer, fadeIn } from "../utils/motions"
+import styles from "../styles"
+
+const World = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer()}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title='| People on the World' textStyles='text-center' />
+      <TitleText
+        title={
+          <>
+            Track friends around you and invite them to play together in the
+            same world
+          </>
+        }
+        textStyles='text-center'
+      />
+
+      <motion.div
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        className='relative mt-[68px] flex w-full h-[550px]'
+      >
+        <img src='/map.png' alt='map' className='w-full h-full object-cover' />
+        <div className='absolute bottom-20 right-[5%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+          <img src='people-01.png' alt='people' className='w-full h-full' />
+        </div>
+        <div className='absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+          <img src='people-02.png' alt='people' className='w-full h-full' />
+        </div>
+        <div className='absolute top-[45%] left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'>
+          <img src='people-03.png' alt='people' className='w-full h-full' />
+        </div>
+        <div className='absolute top-[50%]  w-[260px]'>
+          <img src='card-01.png' alt='people' className='w-full h-full' />
+        </div>
+        <div className='absolute top-[10%] right-[5%] w-[300px]'>
+          <img src='card-02.png' alt='people' className='w-full h-full' />
+        </div>
+      </motion.div>
+    </motion.div>
+  </section>
+)
 
 export default World
