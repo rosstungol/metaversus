f29b3638bbc80ef6aa27cb9c0f02d682abbacbcf
Ross Tungol <ross@proudcloud.io>
2024-03-14 18:08:54 +0800

    Updates Insights component content and styling

--- sections/Insights.tsx
+++ sections/Insights.tsx
@@ -1,5 +1,33 @@
 "use client"
 
-const Insights = () => <section>Insights section</section>
+import { motion } from "framer-motion"
+import { InsightCard, TitleText, TypingText } from "@/components"
+import { staggerContainer } from "../utils/motions"
+import { insights } from "@/constants"
+import styles from "../styles"
+
+const Insights = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer()}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title='| Insight' textStyles='text-center' />
+      <TitleText title='Insight about Metaverse' textStyles='text-center' />
+      <div className='mt-[50px] flex flex-col gap-[30px]'>
+        {insights.map((insight, index) => (
+          <InsightCard
+            key={`insight-${index}`}
+            {...insight}
+            index={index + 1}
+          />
+        ))}
+      </div>
+    </motion.div>
+  </section>
+)
 
 export default Insights
