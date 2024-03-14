6e7266b3f6adf098984b6c55f2efffca8b573ab0
Ross Tungol <ross@proudcloud.io>
2024-03-15 03:33:53 +0800

    Fixes classname code formatting

--- components/InsightCard.tsx
+++ components/InsightCard.tsx
@@ -29,7 +29,10 @@ const InsightCard = ({ imgUrl, title, subtitle, index }: InsightCardProps) => (
           {subtitle}
         </p>
       </div>
-      <div className='lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white'>
+      <div
+        className='lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full
+        bg-transparent border-[1px] border-white'
+      >
         <img
           src='/arrow.svg'
           alt='arrow'
